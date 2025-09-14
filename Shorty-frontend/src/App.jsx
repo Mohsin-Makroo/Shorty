7ca965af8377ad787a578a5f7ef62fb30f6da0c9
Mohsin-Makroo <mohsinmakroo@gmail.com>
2025-09-15 03:14:55 +0530

    vf

--- Shorty-frontend/src/App.jsx
+++ Shorty-frontend/src/App.jsx
@@ -10,13 +10,10 @@ function App() {
     <BrowserRouter>
      <Routes>
       {/* This route contains our main layout */}
-      <Route path="/" element={<Layout />}>
-        {/* These "child" routes will be rendered inside the Layout */}
-        <Route index element={<HomePage />} />
+
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
-      </Route>
     </Routes>
     </BrowserRouter>
    
