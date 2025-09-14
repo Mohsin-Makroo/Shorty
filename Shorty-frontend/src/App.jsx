8ad20a6edf6d5f8b7acb6268c901bdcfe141fade
Mohsin-Makroo <mohsinmakroo@gmail.com>
2025-09-15 03:43:24 +0530

    baarati special ronduu

--- Shorty-frontend/src/App.jsx
+++ Shorty-frontend/src/App.jsx
@@ -7,15 +7,14 @@ import DashboardPage from "./pages/DashboardPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
       </Route>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
+
+      <Route path="login" element={<LoginPage />} />
+      <Route path="signup" element={<SignUpPage />} />
+      <Route path="dashboard" element={<DashboardPage />} />
     </Routes>
-    </BrowserRouter>
   );
 }
