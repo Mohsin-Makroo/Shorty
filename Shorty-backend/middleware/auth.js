daeddfe9e0d1da47f3fcf25bfacb3a44f8712c6d
Mohsin-Makroo <mohsinmakroo@gmail.com>
2025-09-15 18:11:00 +0530

    Final

--- Shorty-backend/middleware/auth.js
+++ Shorty-backend/middleware/auth.js
@@ -1,20 +1,23 @@
 import jwt from 'jsonwebtoken';
 
-// This function acts as a "bouncer" for our protected routes
 export default function (req, res, next) {
-  // Get token from the 'x-auth-token' header
-  const token = req.header('x-auth-token');
-
-  // Check if token doesn't exist
-  if (!token) {
+  // Get token from Authorization header
+  const authHeader = req.header('Authorization');
+  if (!authHeader) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
 
-  // Verify the token
+  // Format of authHeader should be 'Bearer <token>'
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'No token found in authorization header' });
+  }
+
+  // Verify token
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user; // Add user from payload to the request object
-    next(); // Pass control to the next function in the chain
+    req.user = decoded.user; // assign user info from token to request object
+    next();
   } catch (err) {
     res.status(401).json({ message: 'Token is not valid' });
   }
